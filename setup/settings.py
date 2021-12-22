e4d953d26a6a8971e40bd3d277db6e2cf74b775a
GallegosEverson <gallegoseverson@gmail.com>
2021-12-22 19:58:38 -0300

    media urls

--- setup/settings.py
+++ setup/settings.py
@@ -177,6 +177,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- setup/urls.py
+++ setup/urls.py
@@ -5,8 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from django.conf.urls import url
-from django.conf.urls.static import serve
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -14,5 +13,5 @@ urlpatterns = [
     path('draw/', include(urls_draw)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    url(r'^media/(.*)$', serve, {'document_root':settings.STATIC_ROOT}),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
