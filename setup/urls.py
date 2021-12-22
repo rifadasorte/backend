19b7a1c8c38e215495219cb904988c4d19993123
GallegosEverson <gallegoseverson@gmail.com>
2021-12-22 20:03:09 -0300

    media urls

--- setup/urls.py
+++ setup/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('draw/', include(urls_draw)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
