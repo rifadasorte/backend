2d2bd1bcdec5ef91f284017ce74d66e124853195
GallegosEverson <gallegoseverson@gmail.com>
2021-12-22 22:59:27 -0300

    media urls

--- setup/settings.py
+++ setup/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'content')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
