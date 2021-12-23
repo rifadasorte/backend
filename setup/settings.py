692c9a89c8c580cf758101d084ddcaa971c082b3
GallegosEverson <gallegoseverson@gmail.com>
2021-12-23 10:49:51 -0300

    upload to images/

--- setup/settings.py
+++ setup/settings.py
@@ -177,6 +177,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'images')
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
